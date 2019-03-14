20be2b029428ecaec2189bfd3ecc2a4b8dd57794
myociss <yocissms@gmail.com>
2019-03-14 16:06:58 -0500

    fixed setup.py

--- src/freesurgery/gui.py
+++ src/freesurgery/gui.py
@@ -4,36 +4,6 @@ import sys
 from PyQt5.QtWidgets import QMainWindow, QDesktopWidget, QAction, qApp, QApplication
 from PyQt5.QtGui import QIcon
 
-'''
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("FreeSurgery")
-        MainWindow.resize(2000, 1000)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 20))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("FreeSurgery", "FreeSurgery"))
-
-class AppWindow(QtWidgets.QMainWindow):
-
-    def __init__(self):
-        super(AppWindow, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-'''
 
 class AppWindow(QMainWindow):
     
